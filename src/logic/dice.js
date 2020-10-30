4370f45b09a947cf6b43a7a4d75be4c6ebda4802
Xavier Simó <xavier.simo@xing.com>
2020-10-30 21:50:54 +0100

    Add logic to calculate score

--- src/logic/dice.js
+++ src/logic/dice.js
@@ -8,13 +8,81 @@ import {
   doNothing,
 } from "./operations";
 
-const abilityDice = {
-  1: [advantage],
-  2: [advantage],
-  3: [advantage],
-  4: [advantage],
-  5: [advantage],
-  6: [advantage],
-  7: [advantage],
-  8: [advantage],
+const benefitDie = {
+  1: [doNothing],
+  2: [doNothing],
+  3: [increaseSuccess],
+  4: [increaseSuccess, increaseAdvantage],
+  5: [increaseAdvantage, increaseAdvantage],
+  6: [increaseAdvantage],
+};
+
+const complicationDie = {
+  1: [doNothing],
+  2: [doNothing],
+  3: [increaseFailure],
+  4: [increaseFailure],
+  5: [increaseDisadvantage],
+  6: [increaseDisadvantage],
+};
+
+const abilityDie = {
+  1: [doNothing],
+  2: [increaseSuccess],
+  3: [increaseSuccess],
+  4: [increaseSuccess, increaseSuccess],
+  5: [increaseAdvantage],
+  6: [increaseAdvantage],
+  7: [increaseSuccess, increaseAdvantage],
+  8: [increaseAdvantage, increaseAdvantage],
+};
+
+const difficultyDie = {
+  1: [doNothing],
+  2: [increaseFailure],
+  3: [increaseFailure, increaseFailure],
+  4: [increaseDisadvantage],
+  5: [increaseDisadvantage],
+  6: [increaseDisadvantage],
+  7: [increaseDisadvantage, increaseDisadvantage],
+  8: [increaseFailure, increaseDisadvantage],
+};
+
+const proficiencyDie = {
+  1: [doNothing],
+  2: [increaseSuccess],
+  3: [increaseSuccess],
+  4: [increaseSuccess, increaseSuccess],
+  5: [increaseSuccess, increaseSuccess],
+  6: [increaseAdvantage],
+  7: [increaseAdvantage, increaseSuccess],
+  8: [increaseAdvantage, increaseSuccess],
+  9: [increaseAdvantage, increaseSuccess],
+  10: [increaseAdvantage, increaseAdvantage],
+  11: [increaseAdvantage, increaseAdvantage],
+  12: [increaseTriumph],
+};
+
+const challengeDie = {
+  1: [doNothing],
+  2: [increaseFailure],
+  3: [increaseFailure],
+  4: [increaseFailure, increaseFailure],
+  5: [increaseFailure, increaseFailure],
+  6: [increaseDisadvantage],
+  7: [increaseDisadvantage],
+  8: [increaseFailure, increaseDisadvantage],
+  9: [increaseFailure, increaseDisadvantage],
+  10: [increaseDisadvantage, increaseDisadvantage],
+  11: [increaseDisadvantage, increaseDisadvantage],
+  12: [increaseDesperation],
+};
+
+export {
+  benefitDie,
+  complicationDie,
+  abilityDie,
+  difficultyDie,
+  proficiencyDie,
+  challengeDie,
 };
--- src/logic/operations.js
+++ src/logic/operations.js
@@ -20,32 +20,19 @@ const decreaseSuccess = ({ success } = result) => ({
 });
 const increaseFailure = decreaseSuccess;
 
-const increaseTriumph = operationFactory(({ triumph } = result) => ({
+const increaseTriumph = ({ triumph, success } = result) => ({
   ...result,
   triumph: triumph + 1,
-}));
+  success: success + 1,
+});
 
-const increaseDesperation = operationFactory(({ desperation } = result) => ({
+const increaseDesperation = ({ desperation, success } = result) => ({
   ...result,
   desperation: desperation + 1,
-}));
-
-const doNothing = operationFactory((result) => result);
-
-const operationFactory = (operation) => ({
-  fns: [operation],
-  originalFn: operation,
-  times: (x) => {
-    for (let i = 0; i < x; i++) {
-      this.fns = [...this.fns, this.originalFn];
-    }
-    return this;
-  },
-  and: (otherOperation) => {
-    this.fns = [...this.fns, otherOperation.fns];
-    return this;
-  },
+  success: success - 1,
 });
+
+const doNothing = (result) => result;
 
 export {
   increaseAdvantage,
