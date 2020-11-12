630ed38180012f18f12c7dd365c03e6f2e7487b8
Xavier Simó <xavier.simo@xing.com>
2020-11-12 22:05:07 +0100

    Delete what is not used. It is committed, so don't worry

--- src/components/NumberDieForms/constants.js
+++
@@ -1,7 +0,0 @@
-import { AnimatedTriangle, Triangle } from "./Triangle";
-import { AnimatedSquare, Square } from "./Square";
-
-export const animatedDiceByFaces = {
-  4: AnimatedTriangle,
-  6: AnimatedSquare,
-};
